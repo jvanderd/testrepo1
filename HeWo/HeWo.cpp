10c86bdc0acef552515dcd372d5aa6fd1f622644
jvanderd <jvanderd@hills.ccsf.cc.ca.us>
2014-02-04 17:33:34 -0800

    HeWo/HeWo.cpp

---
+++ HeWo/HeWo.cpp
@@ -0,0 +1,23 @@
+/*
+ Project: Hello World
+ Author: CCSF Coders
+ Description: Prints "Hello World" in
+ different spoken languages in cpp
+
+ To compile: g++ HeWo.cpp -o HeWo
+ To run: ./HeWo
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World! - English" << endl; //Coders
+	cout << "Ciao Mondo! - Italian" << endl; //Jason Hoang
+	cout << "Gut Nacht!" << endl;
+
+return 0;
+}
+
+
+
